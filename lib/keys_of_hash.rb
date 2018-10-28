9866a2c9912732a273c803fcb56b380a91ae928c
joneseyit <joneseyit@protonmail.com>
2018-10-28 03:47:52 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,8 @@
 require "pry"
 class Hash
   def keys_of(*args)
-    map {|key, value| binding.pry args.include?(value) ? key : nil }.compact
+    binding.pry
+    map {|key, value|  args.include?(value) ? key : nil }.compact
+    binding.pry
   end
 end
